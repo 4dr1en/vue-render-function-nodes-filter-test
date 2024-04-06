5107fe39764ad71fe9cb1814d92086d1db96ee17
adrien charrier <adrien.charrier@outlook.fr>
2024-04-06 02:59:29 +0200

    format

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,21 +1,21 @@
 <script lang="ts">
 import { defineComponent, h } from 'vue';
 
-    export default defineComponent({
-        render() {
-            const defaultSlot = this.$slots.default ? this.$slots.default() : [];
-            const content = defaultSlot.map((vnode) => {
-                if (vnode.type === 'ul') {
-                    return h('details', { class: 'sub-menu' }, vnode);
-                } else {
-                    return vnode;
-                }
-            });
-            return (
-                h('nav', {
-                    class: 'menu'
-                }, content)
-            )
-        }
-    })
+export default defineComponent({
+    render() {
+        const defaultSlot = this.$slots.default ? this.$slots.default() : [];
+        const content = defaultSlot.map((vnode) => {
+            if (vnode.type === 'ul') {
+                return h('details', { class: 'sub-menu' }, vnode);
+            } else {
+                return vnode;
+            }
+        });
+        return (
+            h('nav', {
+                class: 'menu'
+            }, content)
+        );
+    }
+})
 </script>
